298e4719d1faf87aabce1f15135bf998dc14a38f
Matthew Lewis <matthew.lewis@fatzebra.com.au>
2020-10-30 10:41:35 +1100

    Updating specs

--- spec/db/create_posts.rb
+++ spec/db/create_posts.rb
@@ -1,4 +1,4 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration[4.2]
   def self.up
     execute "CREATE EXTENSION IF NOT EXISTS hstore"
     create_table :posts do |t|
